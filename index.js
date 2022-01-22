35fb8d18494e6ea451e4c30d8e95f54f625b8f01
winsonred <abhijeet7907@gmail.com>
2022-01-23 00:42:18 +0530

    files will be added to git

---
+++ index.js
@@ -0,0 +1,31 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+/*var image2= document.querrySelectorAll("img")[1];
+image1.setAttribute("src","randomImageSource");*/
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomImageSource2 = "images/dice"+randomNumber2+".png";
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "player 1 wins!";
+}
+else if(randomNumber2>randomNumber1)
+{
+  document.querySelector("h1").innerHTML = "player 2 wins!";
+}
+else if(randomNumber2=randomNumber1)
+{
+  document.querySelector("h1").innerHTML = "It's a draw";
+}
